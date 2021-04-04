248adec8bfaa5509f3e6dcd20755140afe87e83c
Victoria-FZ-eng <vzulof@gmail.com>
2021-04-04 16:05:00 +0300

    website created

---
+++ JS/app.js
@@ -0,0 +1,6 @@
+"use strict";
+let userName = prompt ("What\'s Your Name ?");
+let much = prompt ("How much do you Like birds ?");
+let pet = prompt ("What do you prefer parots, love birds or canaries ?" , "Parots, Love birds, Canaries");
+let x = prompt ("How did you hear about us?" , "Social media, A freind");
+alert ("My name is: "+ userName + ".I prefer "+ pet + " I heared about you by "+ x);
--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# Rainbow
-Birds Shop
+# Rainbow The Birds' Shop
+
+This is a web site for a pet store that is specialist in selling various types of the beautiful colorful birds. 
