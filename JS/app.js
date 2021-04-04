5d0ad6c0ab2854de42e5d352e0f2f907c34b79c5
Victoria-FZ-eng <vzulof@gmail.com>
2021-04-04 16:19:34 +0300

    console.log

--- JS/app.js
+++ JS/app.js
@@ -2,5 +2,9 @@
 let userName = prompt ("What\'s Your Name ?");
 let much = prompt ("How much do you Like birds ?");
 let pet = prompt ("What do you prefer parots, love birds or canaries ?" , "Parots, Love birds, Canaries");
-let x = prompt ("How did you hear about us?" , "Social media, A freind");
-alert ("My name is: "+ userName + ".I prefer "+ pet + " I heared about you by "+ x);
+let about = prompt ("How did you hear about us?" , "Social media, A freind");
+alert ("My name is: "+ userName + ".I prefer "+ pet + " I heared about you by "+ about);
+console.log(userName , typeof userName);
+console.log(much , typeof much);
+console.log(pet , typeof pet);
+console.log(about , typeof about);
